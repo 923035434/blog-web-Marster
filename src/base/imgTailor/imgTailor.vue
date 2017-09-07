966f8fa3819a566b863b7556487b5d9b95ad0200
923035434 <923035434@qq.com>
2017-09-07 17:55:05 +0800

    写图片裁缝的算法

--- src/base/imgTailor/imgTailor.vue
+++ src/base/imgTailor/imgTailor.vue
@@ -1,17 +1,29 @@
 <template>
   <div
-    @mousemove="recMouseMove"
-    @mouseup="recMouseUp"
+    @mousemove="MouseMove"
+    @mouseup="MouseUp"
     v-show="show" class="imgTailor">
     <div ref="imgWrapperDom" class="img-wrapper">
       <img class="bottom-img" v-show="imgSrc" :src="imgSrc" >
       <div class="top-img-wrapper">
-        <img class="top-img" v-show="imgSrc" :src="imgSrc">
+        <img ref="topImgDom" class="top-img" v-show="imgSrc" :src="imgSrc">
       </div>
       <div class="filter">
         <div
-          @mousedown="recMouseDown"
+               @mousedown.self="recMouseDown"
           ref="recDom" class="tailor_rec">
+          <div class="topLeft"
+               @mousedown.stop="topLeftMouseDown"
+          ></div>
+          <div class="topRight"
+               @mousedown.stop="topRightMouseDown"
+          ></div>
+          <div class="bottomLeft"
+               @mousedown.stop="bottomLeftMouseDown"
+          ></div>
+          <div class="bottomRight"
+               @mousedown.stop="bottomRightMouseDown"
+          ></div>
         </div>
       </div>
     </div>
@@ -20,6 +32,12 @@
 </template>
 
 <script type="text/ecmascript-6">
+  const moveType = {
+    topLeft: 0,
+    topRight: 1,
+    bottomLeft: 2,
+    bottomRight: 3
+  }
   export default {
     props: {
       imgSrc: {
@@ -33,12 +51,87 @@
     },
     data () {
       return {
-        mouseStar: false
+        mouseStar: false,
+        trangeSizeStar: false
       }
     },
     computed: {
     },
     methods: {
+      _setChangeStar (e) {
+        this.changeStar = {
+          x: e.clientX,
+          y: e.clientY
+        }
+
+        let recDom = this.$refs.recDom
+        let imgWrapperDom = this.$refs.imgWrapperDom
+        let recRect = recDom.getBoundingClientRect()
+        let wrapperRect = imgWrapperDom.getBoundingClientRect()
+
+        this.recLeft = recRect.left - wrapperRect.left
+        this.recTop = recRect.top - wrapperRect.top
+
+        this.recWidth = recDom.clientWidth
+        this.recHeight = recDom.clientHeight
+      },
+      topLeftMouseDown (e) {
+        this.trangeSizeStar = true
+        this.changeType = moveType.topLeft
+        this._setChangeStar(e)
+      },
+      topRightMouseDown (e) {
+        this.trangeSizeStar = true
+        this.changeType = moveType.topRight
+        this._setChangeStar(e)
+      },
+      bottomLeftMouseDown (e) {
+        this.trangeSizeStar = true
+        this.changeType = moveType.bottomLeft
+        this._setChangeStar(e)
+      },
+      bottomRightMouseDown (e) {
+        this.trangeSizeStar = true
+        this.changeType = moveType.bottomRight
+        this._setChangeStar(e)
+      },
+      changeSizeMouseMove (e) {
+        if (!this.trangeSizeStar) {
+          return
+        }
+        let recDom = this.$refs.recDom
+        let imgWrapperDom = this.$refs.imgWrapperDom
+//        let recRect = recDom.getBoundingClientRect()
+//        let wrapperRect = imgWrapperDom.getBoundingClientRect()
+        let width = recDom.clientWidth
+        let height = recDom.clientHeight
+        let left = this.recLeft
+        let top = this.recTop
+        switch (this.changeType) {
+          case moveType.topLeft:
+            width = Math.min(Math.max(this.recWidth + (this.changeStar.x - e.clientX), 0), imgWrapperDom.clientWidth)
+            height = Math.min(Math.max(this.recHeight + (this.changeStar.y - e.clientY), 0), imgWrapperDom.clientHeight)
+            console.log(this.recHeight, this.changeStar.y - e.clientY, width, height)
+            left = Math.min(Math.max(left + (e.clientX - this.changeStar.x), 0), imgWrapperDom.clientWidth - recDom.clientWidth)
+            top = Math.min(Math.max(top + (e.clientY - this.changeStar.y), 0), imgWrapperDom.clientHeight - recDom.clientHeight)
+            console.log('topLeftMove')
+            break
+          case moveType.topRight:
+            top = Math.min(Math.max(top + (e.clientY - this.changeStar.y), 0), imgWrapperDom.clientHeight - recDom.clientHeight)
+            console.log('topRightMove')
+            break
+          case moveType.bottomLeft:
+            console.log('bottomLeftMove')
+            break
+          case moveType.bottomRight:
+            console.log('bottomLeftMove')
+            break
+        }
+        recDom.style.top = top + 'px'
+        recDom.style.left = left + 'px'
+        recDom.style.width = width + 'px'
+        recDom.style.height = height + 'px'
+      },
       recMouseDown (e) {
         this.mouseStar = true
         this.recStarX = e.clientX
@@ -59,17 +152,38 @@
         this.$nextTick(() => {
           let recDom = this.$refs.recDom
           let imgWrapperDom = this.$refs.imgWrapperDom
-          console.log(this.recTop + e.clientX - this.recStarX)
-          console.log(this.recLeft + e.clientY - this.recStarY)
           let left = Math.min(imgWrapperDom.clientWidth - recDom.clientWidth, Math.max(this.recTop + e.clientX - this.recStarX, 0))
           let top = Math.min(imgWrapperDom.clientHeight - recDom.clientHeight, Math.max(this.recLeft + e.clientY - this.recStarY, 0))
           recDom.style.top = top + 'px'
           recDom.style.left = left + 'px'
         })
       },
-      recMouseUp (e) {
+      MouseMove (e) {
+        if (!this.mouseStar && !this.trangeSizeStar) {
+          return
+        }
+        if (this.mouseStar) {
+          this.recMouseMove(e)
+        } else if (this.trangeSizeStar) {
+          this.changeSizeMouseMove(e)
+        }
+      },
+      MouseUp (e) {
         this.mouseStar = false
-        console.log(e)
+        this.trangeSizeStar = false
+        this.clip()
+      },
+      clip () {
+        let topImgDom = this.$refs.topImgDom
+        let recDom = this.$refs.recDom
+        let imgWrapperDom = this.$refs.imgWrapperDom
+        let recRect = recDom.getBoundingClientRect()
+        let wrapperRect = imgWrapperDom.getBoundingClientRect()
+        let top = recRect.top - wrapperRect.top
+        let right = recRect.right - wrapperRect.left
+        let bottom = recRect.bottom - wrapperRect.top
+        let left = recRect.left - wrapperRect.left
+        topImgDom.style.clip = 'rect(' + top + 'px,' + right + 'px,' + bottom + 'px,' + left + 'px)'
       }
     }
   }
@@ -119,6 +233,38 @@
           height: 200px
           border :1px solid #000
           cursor :move
+          .topLeft
+            position : absolute
+            top :-6px
+            left: -6px
+            border: 1px solid #fff
+            width: 6px
+            height: 6px
+            cursor: nw-resize
+          .topRight
+            position : absolute
+            top :-6px
+            right: -6px
+            border: 1px solid #fff
+            width: 6px
+            height: 6px
+            cursor: sw-resize
+          .bottomLeft
+            position : absolute
+            bottom :-6px
+            left: -6px
+            border: 1px solid #fff
+            width: 6px
+            height: 6px
+            cursor: ne-resize
+          .bottomRight
+            position : absolute
+            bottom :-6px
+            right :-6px
+            border: 1px solid #fff
+            width: 6px
+            height: 6px
+            cursor: se-resize
     .ok_btn
       position :absolute
       right: 10%
